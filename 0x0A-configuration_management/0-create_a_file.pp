784c0d9ded034894bcf190d3ab18d6f517857a9a
Emmyy882 <emmaagbaje3@gmail.com>
2023-11-24 23:34:28 +0100

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-# creates a new file in /tmp
+# Creates a file
 file { '/tmp/school':
-  ensure  => 'file',
   mode    => '0744',
-  owner	  => 'www-data',
-  group	  => 'www-data',
-  content => 'I Love Puppet'
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
