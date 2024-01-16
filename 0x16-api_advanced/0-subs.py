4f36c1279bccc368bc2c992b7387d9c952adf187
Emmyy882 <emmaagbaje3@gmail.com>
2024-01-16 22:36:50 +0100

    api-advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''A module containing functions for working with the Reddit API.
+'''
+import requests
+
+
+BASE_URL = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+'''Reddit's base API URL.
+'''
+
+
+def number_of_subscribers(subreddit):
+    '''Retrieves the number of subscribers in a given subreddit.'''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0)',
+            'Geecko/20100101, Firefox/119.0'
+        ])
+    }
+    response = requests.get(BASE_URL,
+                            headers=api_headers,
+                            allow_redirects=False
+                            )
+    if response.status_code == 200:
+        return response.json()['data']['subscribers']
+    return 0
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''A module containing functions for working with the Reddit API.'''
+import requests
+
+
+BASE_URL = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+'''Reddit's base API URL.
+'''
+
+
+def number_of_subscribers(subreddit):
+    '''Retrieves the number of subscribers in a given subreddit.'''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0)',
+            'Geecko/20100101, Firefox/119.0'
+        ])
+    }
+    params = {
+            "limit": 10
+            }
+    response = requests.get(BASE_URL,
+                            headers=api_headers,
+                            params=params,
+                            allow_redirects=False
+                            )
+    if response.status_code == 200:
+        for post in response.json()['data']['children']:
+            print(post['data']['title'])
+    else:
+        print(None)
